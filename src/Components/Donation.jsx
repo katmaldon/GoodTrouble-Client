a3d3c84e6418173ddc3c1015b338e0863571bf4c
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-08-20 22:54:55 -0400

    final final

--- src/Components/Donation.jsx
+++ src/Components/Donation.jsx
@@ -9,7 +9,7 @@ const Book = (props) => {
                 <td><h5 className="title">{props.organization}</h5></td>
                 <td>{props.description}</td>
                 <td><h4 className="url">{props.url}</h4></td>
-                <td><button className="community_button" onClick={() => props.handleDonate(props.id)}>donate now</button></td>
+                <td><button className="community_button" onClick={() => props.handleDonate(props.id)}><a href="https://www.standwithbre.com/#petition">donate now</a></button></td>
             </tr>
 
     );
--- src/Components/Petition.jsx
+++ src/Components/Petition.jsx
@@ -7,7 +7,7 @@ const Petition = (props) => {
                 <td><h5 className="title">{props.cause}</h5></td>
                 <td>{props.description}</td>
                 <td><h4 className="url">{props.url}</h4></td>
-                <td><button className="community_button" onClick={() => props.signNow(props.id)}><a href="https://www.standwithbre.com/#petition">sign now</a></button></td>
+                <td><button className="button" onClick={() => props.signNow(props.id)}><a href="https://www.standwithbre.com/#petition">sign now</a></button></td>
             </tr>
     );
 
